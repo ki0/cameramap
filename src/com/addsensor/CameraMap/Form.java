650189af39fcfd040bf2b2c792ebd7eb2105ea21
Fran Rodriguez <fran.rodriguez@logtrust.com>
2017-02-05 20:07:21 +0100

    Fix image uri path

--- src/com/addsensor/CameraMap/Form.java
+++ src/com/addsensor/CameraMap/Form.java
@@ -1,12 +1,17 @@
 package com.addsensor.CameraMap;
 
 import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.provider.MediaStore.Images.Media;
 import android.util.Log;
@@ -25,6 +30,8 @@ import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.net.URISyntaxException;
 
 //import android.util.Log;
 
@@ -240,10 +247,15 @@ public class Form extends Activity {
 		if (resultCode == RESULT_OK) {
 
 			if (requestCode == GET_CODE) {
-				System.out.println("estoy dentro");
 				// Tomamos la Uri de la imagen seleccionada
 				currImgUri = data.getData();
-				selectedImgPath = getPath(currImgUri);
+				Log.d(Form.TAG, "Current Uri: " + currImgUri);
+				try {
+					selectedImgPath = getPath(getApplicationContext(), currImgUri);
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+				}
+				Log.d(Form.TAG, "Image Path: " + selectedImgPath);
 				if (selectedImgPath != null) {
 					bExplorer.setText(selectedImgPath.subSequence(selectedImgPath.lastIndexOf("/"), selectedImgPath.length()));
 				} else
@@ -252,14 +264,66 @@ public class Form extends Activity {
 		}
 	}
 
-	public String getPath(Uri uri) {
-		String[] projection = {MediaStore.Images.Media.DATA};
-		Cursor cursor = managedQuery(uri, projection, null, null, null);
-		if (cursor != null) {
-			int column_index = cursor.getColumnIndexOrThrow(Media.DATA);
-			cursor.moveToFirst();
-			return cursor.getString(column_index);
-		} else return null;
+	public static String getPath(Context context, Uri uri) throws URISyntaxException {
+		String selection = null;
+		String[] selectionArgs = null;
+		// Uri is different in versions after KITKAT (Android 4.4), we need to
+		if (Build.VERSION.SDK_INT >= 19 && DocumentsContract.isDocumentUri(context.getApplicationContext(), uri)) {
+			if (isExternalStorageDocument(uri)) {
+				final String docId = DocumentsContract.getDocumentId(uri);
+				final String[] split = docId.split(":");
+				return Environment.getExternalStorageDirectory() + "/" + split[1];
+			} else if (isDownloadsDocument(uri)) {
+				final String id = DocumentsContract.getDocumentId(uri);
+				uri = ContentUris.withAppendedId(
+						Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+			} else if (isMediaDocument(uri)) {
+				final String docId = DocumentsContract.getDocumentId(uri);
+				final String[] split = docId.split(":");
+				final String type = split[0];
+				if ("image".equals(type)) {
+					uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+				} else if ("video".equals(type)) {
+					uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+				} else if ("audio".equals(type)) {
+					uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+				}
+				selection = "_id=?";
+				selectionArgs = new String[]{
+						split[1]
+				};
+			}
+		}
+		if ("content".equalsIgnoreCase(uri.getScheme())) {
+			String[] projection = {
+					MediaStore.Images.Media.DATA
+			};
+			Cursor cursor = null;
+			try {
+				cursor = context.getContentResolver()
+						.query(uri, projection, selection, selectionArgs, null);
+				int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+				if (cursor.moveToFirst()) {
+					return cursor.getString(column_index);
+				}
+			} catch (Exception e) {
+			}
+		} else if ("file".equalsIgnoreCase(uri.getScheme())) {
+			return uri.getPath();
+		}
+		return null;
+	}
+
+	public static boolean isExternalStorageDocument(Uri uri) {
+		return "com.android.externalstorage.documents".equals(uri.getAuthority());
+	}
+
+	public static boolean isDownloadsDocument(Uri uri) {
+		return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+	}
+
+	public static boolean isMediaDocument(Uri uri) {
+		return "com.android.providers.media.documents".equals(uri.getAuthority());
 	}
 
 	@Override
